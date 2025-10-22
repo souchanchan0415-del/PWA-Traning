67777cccf5fc87a6a92137d4dac6a5f3d6ab59bf
souchanchan0415-del <souchanchan0415@gmail.com>
2025-10-22 11:45:57 +0900

    sw.js を更新
    バージョンアップ

--- sw.js
+++ sw.js
@@ -12,12 +12,22 @@ const ASSETS = [
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting(); // ← 新しいSWを即座に有効化
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim()) // ← 既存タブも新SWに切替
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(()=>cached)));
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
 });
